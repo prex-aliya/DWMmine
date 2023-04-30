6a5a330885782c01599335ea132f6cd6c04ff02b
prex-aliya <droved-through@proton.me>
2023-04-30 00:46:30 -0400

    update added tatami layout

--- config.h
+++ config.h
@@ -45,14 +45,15 @@ static const Rule rules[] = {
 /* layout(s) -----------------------------------------------{{{ */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = { // TODO: remove symbol
 	/* symbol     arrange function */
+        //{ "|+|",      tatami },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle }, // TODO: Remove this layout
+	//{ "[M]",      monocle }, // TODO: Remove this layout
 };
 /* layout(s) -----------------------------------------------}}} */
 
